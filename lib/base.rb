ab0cae874feeed7ee0187d5571ad6781050f8296
krishnakumar <kumarkrish03@gmail.com>
2016-03-10 22:08:28 +0530

    echo and no cmd fix

--- lib/base.rb
+++ lib/base.rb
@@ -9,23 +9,25 @@ class Base
     when '/hi'
       tube.put({type: 'simple', body: 'Hey, what\'s up?', address: job['address']}.to_json)
     when '/echo'
-      tube.put({type: 'simple', body: job['body'], address: job['address']}.to_json)
+      cmd = job['body'].split(' ')[1..-1].join(' ')
+      tube.put({type: 'simple', body: cmd, address: job['address']}.to_json)
     when '/image'
       tube.put({type: 'image', image: '/home/ubuntu/yowsup-queue-rails-demo/sample.jpg', address: job['address']}.to_json)
     when '/video'
       # To be implemented
     when '/h', '/help'
       #help commands
-      help_text = %Q(
-        [HELP] - Commands
-        /help or /h - Show this message
-        /hi - Replies Hey, what\'s up?
-        /echo - Echo
-        /image - Sends an images
-      )
+      help_text = %Q([HELP] - Commands
+
+/help or /h - Show this message
+/hi - Replies Hey, what\'s up?
+/echo - Echo
+/image - Sends an images)
       tube.put({type: 'simple', body: help_text, address: job['address']}.to_json)
     else
-      tube.put({type: 'simple', body: 'sorry, no command found', address: job['address']}.to_json)
+      if main_command[0] == '/'
+        tube.put({type: 'simple', body: 'sorry, no command found', address: job['address']}.to_json)
+      end
     end
   end
 end
